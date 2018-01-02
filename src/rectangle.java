a1c7c205846705cbe44179359e99ea33261f6082
Gerard2486 <gerard.joseph86@gmail.com>
2018-01-02 11:04:09 +0000

    initial Java Commit

---
+++ src/rectangle.java
@@ -0,0 +1,21 @@
+
+public class Rectangle {
+	public int length;
+	public int width;
+   
+public Rectangle{int length,int width} {
+	this.lenght=length;
+	this.width=width;
+
+	}
+
+public int getArea() {
+	return lenght*width;
+	}
+public int getPerimeter() {
+	return 2*(lenght*width);
+}
+}
+
+
+
